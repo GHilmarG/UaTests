9b9a067a7df2334883466197263dc106118081b1
G. Hilmar Gudmundsson <hilmar.gudmundsson@northumbria.ac.uk>
2018-09-28 14:59:25 +0100

    added Tides

---
+++ Tides/DefineAGlenDistribution.m
@@ -0,0 +1,14 @@
+
+
+function [AGlen,n]=DefineAGlenDistribution(Experiment,coordinates,connectivity,s,b,h,S,B,rho,rhow,Itime,time,CtrlVar)
+
+
+Nnodes=length(coordinates);
+T=-100;
+
+AGlen=AGlenVersusTemp(T)+zeros(Nnodes,1) ;
+
+n=3 ;
+
+end
+
