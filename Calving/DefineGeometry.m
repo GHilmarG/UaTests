4180876fea96c4e45dec42301a24b3bcd4b174f4
G. Hilmar Gudmundsson <hilmar.gudmundsson@northumbria.ac.uk>
2019-06-18 20:03:51 +0100

    Manual thickness changes

--- Calving/DefineGeometry.m
+++ Calving/DefineGeometry.m
@@ -7,19 +7,33 @@ x=MUA.coordinates(:,1); y=MUA.coordinates(:,2);
 alpha=0.;
 B=MismBed(x,y);
 S=B*0;
+s=[]; 
+b=[];
 
 if time< eps
     b=B;
     h0=1000-1000/640e3*x;
     s=b+h0;
-else
-    %Draft=F.h.*(1-F.rho/F.rhow);
-    b=F.b ;
-    s=F.s ;
-
 end
 
-
-
+if contains(UserVar.RunType,"-ManuallyModifyThickness-")
+    
+    if ~isempty(F.GF)
+        
+        s=F.s ;
+        b=F.b ;
+        
+        if CtrlVar.time < 0.01
+            
+            F.GF=IceSheetIceShelves(CtrlVar,MUA,F.GF);
+            CutOff=400e3;
+            I=F.GF.NodesDownstreamOfGroundingLines & x> CutOff ;
+            hMin=CtrlVar.ThickMin+1 ;
+            s(I)=b(I)+hMin;
+            
+        end
+        
+    end
+end
 
 end
