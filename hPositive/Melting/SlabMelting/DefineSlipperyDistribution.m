983b0100fc1915b03717f3daba7b93a94a34ba4d
U-KATLA\Hilmar <ghg@bas.ac.uk>
2020-05-10 12:13:59 +0100

    hPositive

--- hPositive/Melting/SlabMelting/DefineSlipperyDistribution.m
+++ hPositive/Melting/SlabMelting/DefineSlipperyDistribution.m
@@ -5,6 +5,7 @@ function [UserVar,C,m]=DefineSlipperyDistribution(UserVar,CtrlVar,MUA,time,s,b,h
     m=3 ; C=0.0145300017528364 ; % m=3
     %m=1 ; C=1.13263129082193    ; % m=1
 
+    m=3 ; C=100; % creating high velocities 
     C=C+zeros(MUA.Nnodes,1);
     
 end
--- hPositive/Melting/SlabMelting/Ua2D_InitialUserInput.m
+++ hPositive/Melting/SlabMelting/Ua2D_InitialUserInput.m
@@ -7,30 +7,23 @@ function [UserVar,CtrlVar,MeshBoundaryCoordinates]=Ua2D_InitialUserInput(UserVar
 %  Was testing how to generate mesh independent reactions.  I'm doing this in
 %  UaOutputs by using using the mass matrix 
 %
-%  Creating mesh-independent reactinos works fine, and now need to think about
-%  how to change the implementation of the active thickenss constraints to
-%  reflect this.  (August 2016)
-%
+
 
 
 
 UserVar=[];
-CtrlVar.Experiment='GaussMelting';
-%Experiment='UniformMelting';
-CtrlVar.ResetTimeStep=1;                 % 1 if time step should be reset to dt given in the Ua2D_InitialUserInputFile
-
 
 
 CtrlVar.time= 0 ; CtrlVar.dt=1;
-CtrlVar.TimeDependentRun=1 ; CtrlVar.AdaptiveTimeStepping=0 ; 
-CtrlVar.TotalNumberOfForwardRunSteps=5; CtrlVar.TotalTime=100 ;
+CtrlVar.TimeDependentRun=1 ; CtrlVar.AdaptiveTimeStepping=1 ; 
+CtrlVar.TotalNumberOfForwardRunSteps=10000; CtrlVar.TotalTime=1000;
 
-CtrlVar.Restart=0;  
+CtrlVar.UaOutputsDt=1;
+CtrlVar.Restart=1;  
 CtrlVar.WriteRestartFile=1;
-CtrlVar.TriNodes=10 ; 
+CtrlVar.TriNodes=10; 
+CtrlVar.MeshSize=5e3 ; % 10e3 ; 
 
-
-CtrlVar.LinFEbasis=false;  % test parameter, do not change
 
 
 xd=100e3; xu=-100e3 ; yl=100e3 ; yr=-100e3;
@@ -39,17 +32,26 @@ MeshBoundaryCoordinates=flipud([xu yr ; xd yr ; xd yl ; xu yl]);
 CtrlVar.InfoLevelLinSolve=1;
 CtrlVar.PlotXYscale=1000;
 
-CtrlVar.ThickMin=0;
+CtrlVar.ThickMin=0.0;
 
-CtrlVar.ThicknessConstraintsInfoLevel=100 ;
+CtrlVar.ThicknessConstraintsInfoLevel=1 ;
 CtrlVar.ThicknessConstraintsItMax=10  ;     % maximum number of active-set iterations.
 
-CtrlVar.MUA.MassMatrix=true;
+
 CtrlVar.ThicknessConstraints=1;
 
 CtrlVar.ReadInitialMesh=0;    % if true then read FE mesh (coordinates, connectivity) directly from a .mat file 
                               % unless the adaptive meshing option is used, no further meshing is done.
 CtrlVar.ReadInitialMeshFileName='MeshFile.mat'; CtrlVar.SaveInitialMeshFileName='MeshFile.mat';
 CtrlVar.ReadInitialMeshFileName='LocallyRefinedMesh.mat'; CtrlVar.SaveInitialMeshFileName='LocallyRefinedMesh.mat';
+
+
+
+CtrlVar.Experiment=sprintf("GaussMelting-Nod%i-MeshSize%i",CtrlVar.TriNodes,CtrlVar.MeshSize) ; 
+
+CtrlVar.NameOfRestartFiletoWrite="Restart-"+CtrlVar.Experiment+".mat" ; 
+CtrlVar.NameOfRestartFiletoRead=CtrlVar.NameOfRestartFiletoWrite;
+
+
 
 end
