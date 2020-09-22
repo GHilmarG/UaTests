0f5c07abaf529a2bdd78e9111102c9187a8c4957
ghg <ghg@bas.ac.uk>
2020-09-22 11:54:04 +0100

    typose in SlidingLaws DefineSlipperinessDistribution.m corrected

--- SlidingLaws/DefineSlipperyDistribution.m
+++ SlidingLaws/DefineSlipperyDistribution.m
@@ -7,19 +7,19 @@ function [UserVar,C,m,q,mu]=DefineSlipperyDistribution(UserVar,CtrlVar,MUA,time,
     mu=0.5;
     C0=3.16e6^(-m)*1000^m*365.2422*24*60*60;
     
-    C=C0+zeros(MUA.Nnodes,1);
+    CWeertman=C0+zeros(MUA.Nnodes,1);
     
     % N= He(h-h_f) \rho g  (h-h_f)
     
     if CtrlVar.SlidingLaw=="Budd"
         
         h=s-b;
-        hf=(S-B).*rhow./rhow;
-        N=GF.node.*rho.*9.81.*(h-hf);
+        hf=(S-B).*rhow./rho;
+        N=GF.node.*rho.*9.81.*(h-hf)/1000;  % kPa
         N(N<0)=eps; 
         Nqm=N.^(q/m) ;
-        C=C./(Nqm+eps); 
-        
+        CBudd=CWeertman./(Nqm+eps); 
+        C=CBudd ; 
     end
     
     
