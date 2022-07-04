1f2e67d699fb2e4a65e35c4ca0f8f3eb6ef2b3b3
ghg@bas.ac.uk <ghg@bas.ac.uk>
2022-07-04 12:40:17 +0100

    new FC and FA files in Calving PIG-TWG

--- Calving/PIG-TWG/driverForwardIceShelfRemovalRuns.m
+++ Calving/PIG-TWG/driverForwardIceShelfRemovalRuns.m
@@ -1,14 +1,21 @@
 
-
-UserVar.RunType="-FT-P-TWISC0-MR4-SM-5km-Alim" ;  % -P-TWISC- is Thwaites Ice Shelf Calved off, 0km away
-
-
-UserVar.RunType="-FT-P-TWISC0-MR4-SM-5km-Alim-Ca1-Cs100000-Aa1-As100000-" ;  % -P-TWISC- is Thwaites Ice Shelf Calved off, 0km away
-UserVar.RunType="-FT-P-TWIS-MR4-SM-5km-Alim-Ca1-Cs100000-Aa1-As100000-" ;  % not calved off
+Resolution="-5km-" ; 
+% Resolution="-10km-" ; % Inverse files still running (2022-07-04)
+% Resolution="-20km-" ; 
 
 
-CreateAndSaveACInterpolants=false;
+UserVar.RunType="-FT-P-TWISC0-MR4-SM"+Resolution+"Alim-Ca1-Cs100000-Aa1-As100000-" ;  % -P-TWISC- is Thwaites Ice Shelf Calved off, 0km away
+UserVar.RunType="-FT-P-TWIS-MR4-SM"+Resolution+"Alim-Ca1-Cs100000-Aa1-As100000-" ;  % not calved off
+
+% UserVar.RunType="-FT-P-TWIS-MR4-SM"+Resolution+"Alim-Ca1-Cs1000000-Aa1-As1000000-" ;  % not calved off, have not run this regularisation for either Weertman or Conford
+
+
+CreateAndSaveACInterpolants=true;  % But still created if the files with the interpolants do not exist, 
+                                    % but the data files with A and C do.
+
+RunJob=false; 
 BatchJob=false;
+
 
 CtrlVar.TotalTime=500;
 CtrlVar.Inverse.Regularize.logC.ga=str2double(extract(extract(UserVar.RunType,"-Ca"+digitsPattern+"-"),digitsPattern)) ; 
@@ -17,8 +24,12 @@ CtrlVar.Inverse.Regularize.logAGlen.ga=str2double(extract(extract(UserVar.RunTyp
 CtrlVar.Inverse.Regularize.logAGlen.gs=str2double(extract(extract(UserVar.RunType,"-As"+digitsPattern+"-"),digitsPattern)) ; 
 
 CtrlVar.SlidingLaw="Weertman";
+CtrlVar.SlidingLaw="Cornford";
 
-CtrlVar.LevelSetDownstreamAGlen=AGlenVersusTemp(0);  
+CtrlVar.LevelSetDownstreamAGlen=AGlenVersusTemp(0);     
+
+
+%%
 
 pat="-"+digitsPattern+"km";
 MR=str2double(extract(extract(UserVar.RunType,pat),digitsPattern));
@@ -38,25 +49,50 @@ if contains(UserVar.RunType,"-Alim-")
 end
 InvFile=replace(InvFile,".","k");
 
-UserVar.AFile="FA-"+InvFile;
-UserVar.CFile="FC-"+InvFile;
+UserVar.AFile="FA-"+InvFile+".mat";
+UserVar.CFile="FC-"+InvFile+".mat";
+
+AdataFile="InvA-"+InvFile+".mat";
+CdataFile="InvC-"+InvFile+".mat";
 
 
-if CreateAndSaveACInterpolants
-    % Create and save interpolants
-    load("InvA-"+InvFile,"AGlen","xA","yA") ;
-    % load("InvA-Weertman-Ca1-Cs100000-Aa1-As100000-10km","AGlen,"xA,"yA") ;
-    FA=scatteredInterpolant(xA,yA,AGlen);
-    save(UserVar.AFile,"FA");
+if CreateAndSaveACInterpolants ||  (~isfile(UserVar.AFile)  || ~isfile(UserVar.CFile) )
 
-    load("InvC-"+InvFile,"C","xC","yC")
-    FC=scatteredInterpolant(xC,yC,C);
-    save(UserVar.CFile,"FC")
+    
+    % Either 1) always create new interpolants from inverse files, even if the interpolants already
+    %           exist (ie update interpolants),
+    %     or 2) always create  interpolants from inverse files.
+
+    if isfile(AdataFile) && isfile(CdataFile)
+
+        fprintf("Createing FA and FC interpolants.\n")
+        fprintf("FA in %s.\n",UserVar.AFile)
+        fprintf("FC in %s.\n",UserVar.CFile)
+
+        % Create and save interpolants
+        load(AdataFile,"AGlen","xA","yA") ;
+        FA=scatteredInterpolant(xA,yA,AGlen);
+        save(UserVar.AFile,"FA");
+
+        load(CdataFile,"C","xC","yC")
+        FC=scatteredInterpolant(xC,yC,C);
+        save(UserVar.CFile,"FC")
+
+    else
+
+        warning("cant create interpolants as A & C data files do not exist")
+        return
+
+    end
+
+
 end
 
-if BatchJob
-    job1=batch(@Ua,0,{UserVar,CtrlVar},Pool=2) ;
-else
-    Ua(UserVar,CtrlVar)
+if RunJob
+    if BatchJob
+        job1=batch(@Ua,0,{UserVar,CtrlVar},Pool=2) ;
+    else
+        Ua(UserVar,CtrlVar)
+    end
 end
 
