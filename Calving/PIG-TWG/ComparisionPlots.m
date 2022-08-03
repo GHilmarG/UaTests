a67b099a3bea9c1681b53c933c9831967e100a4a
ghg@bas.ac.uk <ghg@bas.ac.uk>
2022-08-03 16:52:34 +0100

    Comparision plots for Thwaites

--- Calving/PIG-TWG/ComparisionPlots.m
+++ Calving/PIG-TWG/ComparisionPlots.m
@@ -29,10 +29,12 @@ Compare="5km C2 Weertman";
 Compare="5km Weertman";  % This one covers 200 years for both runs
 
 
-Compare="5km Cornford";
-Compare="10km Cornford";
+
+
 Compare="10km C2 Cornford";
 Compare="5km C2 Cornford";
+Compare="5km Cornford";
+Compare="10km Cornford";
 
 switch Compare
 
@@ -56,9 +58,10 @@ switch Compare
         % 10km Cornford
         SubString(1)="-Tri3-kH10000-ThickMin0k01-FT-P-TWIS-MR4-SM-TM001-Cornford-10km-Alim-Ca1-Cs100000-Aa1-As100000-kmat";
         SubString(2)="-Tri3-kH10000-ThickMin0k01-FT-P-TWISC0-MR4-SM-TM001-Cornford-10km-Alim-Ca1-Cs100000-Aa1-As100000-kmat";
+        
 
-        % SubString(1)="-Tri3-kH10000-ThickMin0k01-FT-P-Duvh-TWIS-MR4-SM-TM001-Cornford-10km-Alim-Ca1-Cs100000-Aa1-As100000-kmat";
-        % SubString(2)="-Tri3-kH10000-ThickMin0k01-FT-P-Duvh-TWISC0-MR4-SM-TM001-Cornford-10km-Alim-Ca1-Cs100000-Aa1-As100000-kmat";
+        SubString(1)="FT-P-Duvh-TWIS-MR4-SM-TM001-Cornford-10km-Alim-Ca1-Cs100000-Aa1-As100000-";
+        SubString(2)="FT-P-Duvh-TWISC0-MR4-SM-TM001-Cornford-10km-Alim-Ca1-Cs100000-Aa1-As100000-";
 
     case  "10km C2 Cornford"
 
@@ -105,14 +108,14 @@ end
 xb=[-1520 -1445 -1100 -1100 -1350 -1590 -1520] ;yb=[-510  -547  -547 -180 -180   -390 -510];
 xyBoundary=[xb(:) yb(:)]*1000;
 TimeVector=0:10:200;
-TimeVector=0:1:5;
+TimeVector=0:1:50;
 
 
 figVAF=FindOrCreateFigure("Compare");
 figVel=FindOrCreateFigure("Velocity");
 
 
-
+AskForInput=true;
 VAFnode=[] ;
 VAFn=[nan;nan];
 
@@ -145,6 +148,7 @@ for J=1:numel(TimeVector)
 
     if MUA1.Nnodes==MUA2.Nnodes
 
+        VAF2node=VAF2.node; 
         dVAFnode=VAF2.node-VAF1.node ;
         ub2=F2.ub;
         vb2=F2.vb;
@@ -152,7 +156,7 @@ for J=1:numel(TimeVector)
     else
 
         % map variables onto mesh1
-        [RunInfo,VAF2node,ub2,vb2]=MapNodalVariablesFromMesh1ToMesh2(CtrlVar,[],MUA2,MUA1,[],VAF2.node,F2.ub,F2,vb);
+        [RunInfo,VAF2node,ub2,vb2]=MapNodalVariablesFromMesh1ToMesh2(CtrlVar,[],MUA2,MUA1,[],VAF2.node,F2.ub,F2.vb);
         dVAFnode=VAF2node-VAF1.node ;
 
 
@@ -168,7 +172,7 @@ for J=1:numel(TimeVector)
     SLR=-dVAFGt/362 ; 
 
 
-    title(sprintf("%s: VAF diff at time %3.1f, dVAF=%g Gt, SLR=%5.2f (mm)",Compare,F1.time,(VAF2.Total-VAF1.Total)/1e9,SLR),Interpreter="latex" )
+    title(sprintf("%s: VAF diff at time %3.1f  \n dVAF=%g Gt, SLR=%5.2f (mm)",Compare,F1.time,(VAF2.Total-VAF1.Total)/1e9,SLR),Interpreter="latex" )
     fprintf("t=%f \t VAF1=%f \t VAF2=%f \n ",F1.time,VAF1.Total/1e9,VAF2.Total/1e9)
     hold on
     PlotGroundingLines(CtrlVar,MUA2,F2.GF,[],[],[],color="r",LineStyle="-",LineWidth=2);
@@ -198,11 +202,13 @@ for J=1:numel(TimeVector)
     dub(F1.GF.NodesDownstreamOfGroundingLines)=NaN;
     dvb(F1.GF.NodesDownstreamOfGroundingLines)=NaN;
 
+    dub(VAF2node<eps)=NaN;
+    dvb(VAF2node<eps)=NaN;
 
     CtrlVar.VelColorMap=jet(100) ;
     cbar=QuiverColorGHG(F1.x,F1.y,dub,dvb,CtrlVar) ;
     title(cbar,"(m/a)")
-    title(sprintf("%s \n velocities difference at t=%f",Compare,CtrlVar.time),Interpreter="latex")
+    title(sprintf("%s \n velocities difference at t=%3.1f",Compare,CtrlVar.time),Interpreter="latex")
 
     hold on
     PlotGroundingLines(CtrlVar,MUA1,F1.GF,[],[],[],color="r",LineStyle="-",LineWidth=1);
@@ -214,10 +220,15 @@ for J=1:numel(TimeVector)
     figVel.Position=[1050 500 1000 800];
 
     drawnow
-    prompt = "Do you want more? Y/N [Y]: ";
-    txt = input("Press RET for continue","s");
-    if ~isempty(txt)
-        break
+
+    if AskForInput
+        prompt = " Y/N/C [Y]: ";
+        txt = input("Ret to continue, N for break, C for continous: ","s");
+        if txt=="C" || txt=="c"
+            AskForInput=false;
+        elseif txt=="N" || txt=="n"
+            break
+        end
     end
 
 
