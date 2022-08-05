80c5311f1ff36d9cff955a4ddf4423815108de1a
ghg@bas.ac.uk <ghg@bas.ac.uk>
2022-08-05 11:23:19 +0100

    new 5km runs

--- Calving/PIG-TWG/driverForwardIceShelfRemovalRuns.m
+++ Calving/PIG-TWG/driverForwardIceShelfRemovalRuns.m
@@ -10,8 +10,10 @@
 % Job70: FT-P-TWISC0-MR4-SM-TM001-Cornford-10km-Alim-Ca1-Cs100000-Aa1-As100000-                     t=94.7 dt=0.01    *TWIS99.73/117
 % Job71: FT-P-Duvh-TWISC0-MR4-SM-TM001-Cornford-10km-Alim-Ca1-Cs100000-Aa1-As100000-                t=102  dt=0.01    116/131
 % Job72: FT-P-Duvh-TWIS-MR4-SM-TM001-Cornford-10km-Alim-Ca1-Cs100000-Aa1-As100000-                  t=186 dt=0.02      200.6/212
+
 % Job74: FT-P-TWIS-MR4-SM-Cornford-5km-Alim-Ca1-Cs100000-Aa1-As100000-                              t=73.9 dt=0.1    91.29/95.95/109.97
-% Job75: FT-P-TWISC0-MR4-SM-Cornford-5km-Alim-Ca1-Cs100000-Aa1-As100000-                            t=17 dt=0.003 (resubmitted on 27 July)
+% Job75: FT-P-TWISC0-MR4-SM-Cornford-5km-Alim-Ca1-Cs100000-Aa1-As100000-                            t=17 dt=0.003    59.3
+
 % Job77: FT-P-Duvh-TWISC2-MR4-SM-TM001-Cornford-10km-Alim-Ca1-Cs100000-Aa1-As100000                 t=200, done
 % Job78: FT-P-Duvh-TWISC0MGL-MR4-SM-TM001-Cornford-10km-Alim-Ca1-Cs100000-Aa1-As100000-             t=142.4 dt=0.014    159.74         169  200(done)
 % Job79: 
@@ -27,9 +29,12 @@
 Resolution="-10km-" ; 
 CtrlVar.SlidingLaw="Cornford";
 CtrlVar.SlidingLaw="Weertman";
-C="C0" ;
+C="C0" ; Duvh="-Duvh-" ;
 
-Resolution="-5km-" ;  CtrlVar.SlidingLaw="Cornford"; C="C0" ;
+Resolution="-5km-" ;  CtrlVar.SlidingLaw="Cornford"; C="" ;     Duvh="-Duvh-" ;  % missing
+Resolution="-5km-" ;  CtrlVar.SlidingLaw="Cornford"; C="" ;     Duvh="" ;  % missing
+Resolution="-5km-" ;  CtrlVar.SlidingLaw="Cornford"; C="C0" ;   Duvh="-Duvh-" ;  % missing
+Resolution="-5km-" ;  CtrlVar.SlidingLaw="Cornford"; C="C0" ;   Duvh="" ;  % running
 
 
 CreateAndSaveACInterpolants=false;  % But still created if the files with the interpolants do not exist, 
@@ -62,6 +67,10 @@ UserVar.RunType="-FT-P-TWISC0-MR4-SM-TM001-Cornford"+Resolution+"Alim-Ca1-Cs1000
 
 UserVar.RunType="-FT-P-Duvh-TWIS"+C+"-MR4-SM-TM001-"+CtrlVar.SlidingLaw+Resolution+"Alim-Ca1-Cs100000-Aa1-As100000-" ;    % calved off with smaller ThickMin initially submitted 2022-07-10
 
+UserVar.RunType="-FT-P-"+Duvh+"-TWIS"+C+"-MR4-SM-TM001-"+CtrlVar.SlidingLaw+Resolution+"Alim-Ca1-Cs100000-Aa1-As100000-" ;    % calved off with smaller ThickMin initially submitted 2022-07-10
+
+
+
 % UserVar.RunType="-FT-P-Duvh-TWISC0-MR4-SM-TM001-Cornford"+Resolution+"Alim-Ca1-Cs100000-Aa1-As100000-" ;    % calved off with smaller ThickMin and automated LSF ele deactivation, 
                                                                                                             %  initially submitted 2022-07-10 for 10km
                                                                                                             %  then for 5km on  2022-07-27 on DESKTOP-G5TCRTD
@@ -87,7 +96,7 @@ UserVar.RunType="-FT-P-Duvh-TWIS"+C+"-MR4-SM-TM001-"+CtrlVar.SlidingLaw+Resoluti
                                                                                                      
 %%
 
-
+UserVar.RunType=replace(UserVar.RunType,"--","-");
  
 
 CtrlVar.TotalTime=200;
