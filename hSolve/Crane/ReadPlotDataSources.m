dcf684820604f16de8fa99e98b1d5af1b2fb28cb
U-C23000099\pcnj6 <ghg@bas.ac.uk>
2023-03-02 19:13:36 +0000

    hsolve input files for Crane test case

--- hSolve/Crane/ReadPlotDataSources.m
+++ hSolve/Crane/ReadPlotDataSources.m
@@ -19,223 +19,245 @@ elseif contains(hostname,"C23000099")
 end
 
 
+isReadData=true;
+isDefinePolygonArea=false;
+isSaveInterpolants=false;
 
-%%  Reading ITS velocity data.
+if isReadData
 
-wdir=OneDriveFolder+"ITS_LIVE\";
 
-filenameITS="ANT_G0240_1996.nc" ; 
-filenameITS="ANT_G0240_2018.nc" ; 
-filenameITS="ANT_G0120_0000.nc";
+    %%  Reading ITS velocity data.
 
-fprintf(" Reading %s  ...",wdir+filenameITS)
+    wdir=OneDriveFolder+"ITS_LIVE\";
 
-finfo = ncinfo(wdir+filenameITS) ;
+    filenameITS="ANT_G0240_1996.nc" ;
+    filenameITS="ANT_G0240_2018.nc" ;
+    filenameITS="ANT_G0120_0000.nc";
 
-xITS=ncread(wdir+filenameITS,"x");   fprintf("x ...")
-yITS=ncread(wdir+filenameITS,"y");    fprintf("y ...")    % these y values are in decending order
-vx=ncread(wdir+filenameITS,"vx");   fprintf("vx ...")
-vy=ncread(wdir+filenameITS,"vy");   fprintf("vy ...")
-rock=ncread(wdir+filenameITS,"rock");   fprintf("rock ...")
-ice=ncread(wdir+filenameITS,"ice");   fprintf("ice ...")
-vxErr=ncread(wdir+filenameITS,"vx_err");   fprintf("vx_err ...")
-vyErr=ncread(wdir+filenameITS,"vy_err");   fprintf("vy_err ...")
+    fprintf(" Reading %s  ...",wdir+filenameITS)
 
-fprintf("done. \n")
+    finfo = ncinfo(wdir+filenameITS) ;
 
-% y values are in decending order, so flip
+    xITS=ncread(wdir+filenameITS,"x");   fprintf("x ...")
+    yITS=ncread(wdir+filenameITS,"y");    fprintf("y ...")    % these y values are in decending order
+    vx=ncread(wdir+filenameITS,"vx");   fprintf("vx ...")
+    vy=ncread(wdir+filenameITS,"vy");   fprintf("vy ...")
+    rock=ncread(wdir+filenameITS,"rock");   fprintf("rock ...")
+    ice=ncread(wdir+filenameITS,"ice");   fprintf("ice ...")
+    ocean=ncread(wdir+filenameITS,"ocean");   fprintf("ocean ...")
+    vxErr=ncread(wdir+filenameITS,"vx_err");   fprintf("vx_err ...")
+    vyErr=ncread(wdir+filenameITS,"vy_err");   fprintf("vy_err ...")
 
-yITS=flipud(yITS);
-vx=fliplr(vx);
-vy=fliplr(vy);  
-rock=fliplr(rock);
-ice=fliplr(ice);
-vxErr=fliplr(vxErr);
-vyErr=fliplr(vyErr);
+    fprintf("done. \n")
 
+    % y values are in decending order, so flip
 
-% change the no data convention, set errors to 1e10 and velocities to zeros at
-% locations where no data is available
-I=vx==-32767;  vx(I)=0;  vy(I)=0; vxErr(I)=1e10 ; vyErr(I)=1e10;  
+    yITS=flipud(yITS);
+    vx=fliplr(vx);
+    vy=fliplr(vy);
+    rock=fliplr(rock);
+    ice=fliplr(ice);
+    vxErr=fliplr(vxErr);
+    vyErr=fliplr(vyErr);
 
-%% Read REMA 
-% y corresponds to the  first index in the original data set
-%
 
-P=OneDriveFolder+"REMA\rema_mosaci_1km_v2k0_filled_cop30\";
-filenameREMA=P+"rema_mosaic_1km_v2.0_filled_cop30_dem.tif";
+    % change the no data convention, set errors to 1e10 and velocities to zeros at
+    % locations where no data is available
+    I=vx==-32767;  vx(I)=0;  vy(I)=0; vxErr(I)=1e10 ; vyErr(I)=1e10;
 
+    %% Read REMA
+    % y corresponds to the  first index in the original data set
+    %
 
-% P=OneDriveFolder+"REMA\rema_mosaic_100m_v2k0_filled_cop30\";
-% filenameREMA=P+"rema_mosaic_100m_v2.0_filled_cop30_dem.tif";
+    P=OneDriveFolder+"REMA\rema_mosaci_1km_v2k0_filled_cop30\";
+    filenameREMA=P+"rema_mosaic_1km_v2.0_filled_cop30_dem.tif";
 
-fprintf("Reading REMA...    ")
-[sREMA,R] = readgeoraster(filenameREMA) ;
-fprintf("done\n")
 
+    % P=OneDriveFolder+"REMA\rema_mosaic_100m_v2k0_filled_cop30\";
+    % filenameREMA=P+"rema_mosaic_100m_v2.0_filled_cop30_dem.tif";
 
-dx=(R.XWorldLimits(2)-R.XWorldLimits(1))/(R.RasterSize(2)) ;
-dy=(R.YWorldLimits(2)-R.YWorldLimits(1))/(R.RasterSize(1)) ;
+    fprintf("Reading REMA...    ")
+    [sREMA,R] = readgeoraster(filenameREMA) ;
+    fprintf("done\n")
 
-xRema=(R.XWorldLimits(1)+dx/2):dx:(R.XWorldLimits(2)-dx/2); 
-yRema=(R.YWorldLimits(1)+dy/2):dy:(R.YWorldLimits(2)-dy/2);   % I guess this is what they did (why not just include xps and yps in the data set?!) 
 
-xRema=xRema(:) ; yRema=yRema(:);
+    dx=(R.XWorldLimits(2)-R.XWorldLimits(1))/(R.RasterSize(2)) ;
+    dy=(R.YWorldLimits(2)-R.YWorldLimits(1))/(R.RasterSize(1)) ;
 
+    xRema=(R.XWorldLimits(1)+dx/2):dx:(R.XWorldLimits(2)-dx/2);
+    yRema=(R.YWorldLimits(1)+dy/2):dy:(R.YWorldLimits(2)-dy/2);   % I guess this is what they did (why not just include xps and yps in the data set?!)
 
-%[X,Y]=ndgrid(single(xRema),single(yRema)) ;
+    xRema=xRema(:) ; yRema=yRema(:);
 
-Fs=griddedInterpolant({xRema,yRema},single(rot90(sREMA,3))); 
 
-%% Define region of interest
+    %[X,Y]=ndgrid(single(xRema),single(yRema)) ;
 
-Region=[-2440 -2320 1200 1320]*1000 ; % This defines the region considered, in xps,yps coordinates
+    Fs=griddedInterpolant({xRema,yRema},single(rot90(sREMA,3)));
 
+    %% Define region of interest
 
+    Region=[-2440 -2320 1200 1320]*1000 ; % This defines the region considered, in xps,yps coordinates
 
-%% Plotting velocities
 
 
+    %% Plotting velocities
 
-fprintf("plotting Antarctic velocities.\n")
-fig=FindOrCreateFigure("Velocities");
 
-istep=20;
-Par.VelPlotIntervalSpacing='log10' ;
 
-QuiverColorGHG(xITS(1:istep:end)/1000,yITS(1:istep:end)/1000,vx(1:istep:end,1:istep:end),vy(1:istep:end,1:istep:end),Par) ;
+    fprintf("plotting Antarctic velocities.\n")
+    fig=FindOrCreateFigure("Velocities");
 
-[cGL,yGL]=PlotGroundingLines([],"Bedmachine",[],[],[],[],"k");
-xlabel("xps (km)",Interpreter="latex")  ; ylabel("yps (km)",Interpreter="latex")
-title(replace(sprintf("%s",filenameITS),"_","-"))
+    istep=20;
+    Par.VelPlotIntervalSpacing='log10' ;
 
-% Plotting velocity errors
-vErr=sqrt(vxErr.*vxErr+vyErr.*vyErr);
+    QuiverColorGHG(xITS(1:istep:end)/1000,yITS(1:istep:end)/1000,vx(1:istep:end,1:istep:end),vy(1:istep:end,1:istep:end),Par) ;
 
-FindOrCreateFigure("vErr")
-istep=200;
-contourf(XITS(1:istep:end,1:istep:end)/1000,YITS(1:istep:end,1:istep:end)/1000,vErr(1:istep:end,1:istep:end)') ;
-cbar=colorbar;
-axis equal
-xlabel("xps (km)",Interpreter="latex")  ; ylabel("yps (km)",Interpreter="latex")
+    [cGL,yGL]=PlotGroundingLines([],"Bedmachine",[],[],[],[],"k");
+    xlabel("xps (km)",Interpreter="latex")  ; ylabel("yps (km)",Interpreter="latex")
+    title(replace(sprintf("%s",filenameITS),"_","-"))
 
-%%
+    % Plotting velocity errors
+    vErr=sqrt(vxErr.*vxErr+vyErr.*vyErr);
 
-fprintf("plotting regional Antarctic velocities.\n")
+    [XITS,YITS]=meshgrid(xITS,yITS);
 
-fig=FindOrCreateFigure("Regional Velocities");
 
+    FindOrCreateFigure("vErr")
+    istep=200;
+    contourf(XITS(1:istep:end,1:istep:end)/1000,YITS(1:istep:end,1:istep:end)/1000,vErr(1:istep:end,1:istep:end)') ;
+    cbar=colorbar;
+    axis equal
+    xlabel("xps (km)",Interpreter="latex")  ; ylabel("yps (km)",Interpreter="latex")
 
-ix=xITS>Region(1)    & xITS < Region(2) ;
-iy=yITS>Region(3)    & yITS < Region(4) ; 
+    %%
 
+    fprintf("plotting regional Antarctic velocities.\n")
 
-xRegion=xITS(ix) ;   % These are the x,y vectors of the region
-yRegion=yITS(iy) ; 
+    fig=FindOrCreateFigure("Regional Velocities");
 
-vxRegion=vx(ix,iy);  vyRegion=vy(ix,iy);
-rockRegion=rock(ix,iy); iceRegion=ice(ix,iy);
-vxErrRegion=vxErr(ix,iy) ;  vyErrRegion=vyErr(ix,iy) ; 
 
-axis(Region)
-Par.VelPlotIntervalSpacing='log10' ;
-QuiverColorGHG(xRegion/1000,yRegion/1000,vxRegion,vyRegion,Par) ;
-[cGL,yGL]=PlotGroundingLines([],"Bedmachine",[],[],[],[],"k");
-axis(Region/1000) ; 
-xlabel("xps (km)",Interpreter="latex")  ; ylabel("yps (km)",Interpreter="latex")
-title(replace(sprintf("%s",filenameITS),"_","-"))
+    ix=xITS>Region(1)    & xITS < Region(2) ;
+    iy=yITS>Region(3)    & yITS < Region(4) ;
 
 
+    xRegion=xITS(ix) ;   % These are the x,y vectors of the region
+    yRegion=yITS(iy) ;
 
+    vxRegion=vx(ix,iy);  vyRegion=vy(ix,iy);
+    rockRegion=rock(ix,iy); iceRegion=ice(ix,iy);    oceanRegion=ocean(ix,iy); 
+    vxErrRegion=vxErr(ix,iy) ;  vyErrRegion=vyErr(ix,iy) ;
 
+    axis(Region)
+    Par.VelPlotIntervalSpacing='log10' ;
+    QuiverColorGHG(xRegion/1000,yRegion/1000,vxRegion,vyRegion,Par) ;
+    [cGL,yGL]=PlotGroundingLines([],"Bedmachine",[],[],[],[],"k");
+    axis(Region/1000) ;
+    xlabel("xps (km)",Interpreter="latex")  ; ylabel("yps (km)",Interpreter="latex")
+    title(replace(sprintf("%s",filenameITS),"_","-"))
 
 
-%%
 
 
-[XRegion,YRegion]=ndgrid(xRegion,yRegion);
 
-sRegion=Fs(XRegion,YRegion) ;
 
-FindOrCreateFigure("REMA") ;
-contourf(XRegion/1000,YRegion/1000,sRegion) ; colorbar
-hold on ; [cGL,yGL]=PlotGroundingLines([],"Bedmachine",[],[],[],[],"w");
-xlabel("xps (km)",Interpreter="latex")  ; ylabel("yps (km)",Interpreter="latex")
+    %%
 
 
-%% Surface mass balance
+    [XRegion,YRegion]=ndgrid(xRegion,yRegion);
 
+    sRegion=Fs(XRegion,YRegion) ;
 
+    FindOrCreateFigure("REMA") ;
+    contourf(XRegion/1000,YRegion/1000,sRegion) ; colorbar
+    hold on ; [cGL,yGL]=PlotGroundingLines([],"Bedmachine",[],[],[],[],"w");
+    xlabel("xps (km)",Interpreter="latex")  ; ylabel("yps (km)",Interpreter="latex")
 
-P=OneDriveFolder+"Antarctic Accumulation Maps\";
-filenameRACMO="SMB_RACMO2.3_1979_2011.nc" ;
 
+    %% Surface mass balance
 
 
-LAT=ncread(P+filenameRACMO,'lat2d')';
-LON=ncread(P+filenameRACMO,'lon2d')';
-SMB=ncread(P+filenameRACMO,'SMB')';
 
+    P=OneDriveFolder+"Antarctic Accumulation Maps\";
+    filenameRACMO="SMB_RACMO2.3_1979_2011.nc" ;
 
-[X, Y]=ll2xy(LAT,LON); %transform to your Polar stereo projection
 
-disp('interpolating (assuming rho_ice = 917 kg/m^3)');
-rho_ice = 917;
 
-acc=SMB/rho_ice ; 
+    LAT=ncread(P+filenameRACMO,'lat2d')';
+    LON=ncread(P+filenameRACMO,'lon2d')';
+    SMB=ncread(P+filenameRACMO,'SMB')';
 
-% smb = griddata(X,Y,SMB,x,y) / 917; %in m/yr
 
-Fas=scatteredInterpolant(X(:),Y(:),acc(:)); 
+    [X, Y]=ll2xy(LAT,LON); %transform to your Polar stereo projection
 
-% map on region
-asRegion=Fas(XRegion,YRegion) ;
+    disp('interpolating (assuming rho_ice = 917 kg/m^3)');
+    rho_ice = 917;
 
-%%
-FindOrCreateFigure("as Region") ;
-contourf(XRegion/1000,YRegion/1000,asRegion) ; cbar=colorbar ; 
-title(cbar,"(m/yr)")
-hold on ; [cGL,yGL]=PlotGroundingLines([],"Bedmachine",[],[],[],[],"w");
-xlabel("xps (km)",Interpreter="latex")  ; ylabel("yps (km)",Interpreter="latex")
+    acc=SMB/rho_ice ;
 
+    % smb = griddata(X,Y,SMB,x,y) / 917; %in m/yr
 
-%%
-FindOrCreateFigure("rock Region") ;
-contourf(XRegion/1000,YRegion/1000,rockRegion) ; cbar=colorbar ; 
-hold on ; [cGL,yGL]=PlotGroundingLines([],"Bedmachine",[],[],[],[],"w");
-xlabel("xps (km)",Interpreter="latex")  ; ylabel("yps (km)",Interpreter="latex")
+    Fas=scatteredInterpolant(X(:),Y(:),acc(:));
 
-FindOrCreateFigure("ice Region") ;
-contourf(XRegion/1000,YRegion/1000,iceRegion) ; cbar=colorbar ; 
-hold on ; [cGL,yGL]=PlotGroundingLines([],"Bedmachine",[],[],[],[],"w");
-xlabel("xps (km)",Interpreter="latex")  ; ylabel("yps (km)",Interpreter="latex")
+    % map on region
+    asRegion=Fas(XRegion,YRegion) ;
 
+    %%
+    FindOrCreateFigure("as Region") ;
+    contourf(XRegion/1000,YRegion/1000,asRegion) ; cbar=colorbar ;
+    title(cbar,"(m/yr)")
+    hold on ; [cGL,yGL]=PlotGroundingLines([],"Bedmachine",[],[],[],[],"w");
+    xlabel("xps (km)",Interpreter="latex")  ; ylabel("yps (km)",Interpreter="latex")
 
 
-%% Regional interpolants
-%
-Fs=griddedInterpolant({xRegion,yRegion},sRegion) ;
-Fvx=griddedInterpolant({xRegion,yRegion},vxRegion) ;
-Fvy=griddedInterpolant({xRegion,yRegion},vyRegion) ;
-FvxError=griddedInterpolant({xRegion,yRegion},vxErrRegion) ;
-FvyError=griddedInterpolant({xRegion,yRegion},vyErrRegion) ;
-Fas=griddedInterpolant({xRegion,yRegion},asRegion) ;
-Frock=griddedInterpolant({xRegion,yRegion},single(rockRegion)) ;
+    %%
+    FindOrCreateFigure("rock Region") ;
+    contourf(XRegion/1000,YRegion/1000,rockRegion) ; cbar=colorbar ;
+    hold on ; [cGL,yGL]=PlotGroundingLines([],"Bedmachine",[],[],[],[],"w");
+    xlabel("xps (km)",Interpreter="latex")  ; ylabel("yps (km)",Interpreter="latex")
 
-Fice=griddedInterpolant({xRegion,yRegion},single(iceRegion)) ;
+    FindOrCreateFigure("ice Region") ;
+    contourf(XRegion/1000,YRegion/1000,iceRegion) ; cbar=colorbar ;
+    hold on ; [cGL,yGL]=PlotGroundingLines([],"Bedmachine",[],[],[],[],"w");
+    xlabel("xps (km)",Interpreter="latex")  ; ylabel("yps (km)",Interpreter="latex")
 
+end 
 
-%% Define polygon area of interest interactivily
 
+if isSaveInterpolants
 
+    %% Regional interpolants
+    %
+    Fs=griddedInterpolant({xRegion,yRegion},sRegion) ;
+    Fvx=griddedInterpolant({xRegion,yRegion},vxRegion) ;
+    Fvy=griddedInterpolant({xRegion,yRegion},vyRegion) ;
+    FvxError=griddedInterpolant({xRegion,yRegion},vxErrRegion) ;
+    FvyError=griddedInterpolant({xRegion,yRegion},vyErrRegion) ;
+    Fas=griddedInterpolant({xRegion,yRegion},asRegion) ;
+    Frock=griddedInterpolant({xRegion,yRegion},single(rockRegion)) ;
+    Focean=griddedInterpolant({xRegion,yRegion},single(oceanRegion)) ;
+    Fice=griddedInterpolant({xRegion,yRegion},single(iceRegion)) ;
 
-[xp,yp]=GetMousePoints;
 
-save("DomainOutline","xp","yp")
+    save("RegionalInterpolants","Fs","Fvx","Fvy","FvxError","FvyError","Fas","Frock","Fice","Focean")
 
 
 
+end
 
+
+if isDefinePolygonArea
+
+    %% Define polygon area of interest interactivily
+
+
+
+    [xp,yp]=GetMousePoints;
+
+    save("DomainOutline","xp","yp")
+
+
+
+end
 
 
 
