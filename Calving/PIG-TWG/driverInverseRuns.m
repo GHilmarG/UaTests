60938f9ccacc85ef82486dab1af3b59283ec3e16
U-C23000099\pcnj6 <ghg@bas.ac.uk>
2023-03-20 10:03:10 +0000

    some old mat files deleted

--- Calving/PIG-TWG/driverInverseRuns.m
+++ Calving/PIG-TWG/driverInverseRuns.m
@@ -33,6 +33,7 @@ UserVar.RunType='Inverse-MatOpt-Alim-Clim-Umbi-';  UserVar.MeshResolution=5e3; I
 UserVar.RunType='Inverse-MatOpt-uvdhdt-Alim-Clim-Umbi-';  UserVar.MeshResolution=5e3; IRange=5:5 ; JRange=5:5 ;
 UserVar.RunType='Inverse-MatOpt-uvdhdt-Alim-Clim-Umbi-';  UserVar.MeshResolution=20e3; IRange=5:5 ; JRange=5:5 ;
 
+UserVar.RunType='Inverse-MatOpt-uvdhdt-Alim-Clim-Cornford-';  UserVar.MeshResolution=2.5e3; IRange=5:5 ; JRange=5:5 ;
 
 UserVar.RunType='Inverse-MatOpt-uvdhdt-Alim-Clim-Cornford-';  UserVar.MeshResolution=5e3; IRange=5:5 ; JRange=5:5 ;
 UserVar.RunType='Inverse-MatOpt-uvdhdt-Alim-Clim-Cornford-';  UserVar.MeshResolution=20e3; IRange=5:5 ; JRange=5:5 ;
@@ -141,7 +142,7 @@ else
  
  
 
-    CtrlVar.Inverse.Iterations=2500;
+    CtrlVar.Inverse.Iterations=3;
     CtrlVar.Inverse.Regularize.logC.ga=1;
     CtrlVar.Inverse.Regularize.logC.gs=100000  ; % 100000000  ; % 10000000 (c); %   5000000 (c) ; %     1000000 (c) ;
 
