1fb2f8cd713e6f7354453d2d51f3202e246f5225
U-C23000099\pcnj6 <ghg@bas.ac.uk>
2023-06-16 14:33:43 +0100

    testing back with direct descent option

--- Calving/PIG-TWG/TestBack.m
+++ Calving/PIG-TWG/TestBack.m
@@ -24,27 +24,38 @@ slope0Test = -(gradJ'*dxNewton)/Normalisation ;
 CtrlVar.uvMinimisationQuantity="Force Residuals" ;
 [gamma,r,BackTrackInfo]=BackTracking(slope0,1,r0,r1,Func,CtrlVar);
 
-%%
+%%  Direct descent direction with backtracking
 % Newton direction is [dub;dvb;dl]
 % Steepest descent is [frhs; grhs]
+
+
 
 dJdu=frhs(1:MUA.Nnodes);
 dJdv=frhs(MUA.Nnodes+1:2*MUA.Nnodes);
 dJdl=grhs ;
 
+R=[dJdu;dJdv;dJdl]; 
+[nL,mL]=size(L);
+C=sparse(nL,nL);
+H=[Kuv L' ;L -C] ;
 
- 
+slope0Descent=2*R'*H*R/Normalisation ;
 
 FuncDescent=@(gamma) CalcCostFunctionNR(UserVar,RunInfo,CtrlVar,MUA,gamma,F,fext0,L,l,cuv,dJdu,dJdv,dJdl) ;
 
 
-gamma=0 ; [r0,UserVar,RunInfo,rForce0,rWork0,D20frhs,grhs]=FuncDescent(gamma);
+gamma=0 ; [r0,UserVar,RunInfo,rForce0,rWork0,D20,frhs,grhs]=FuncDescent(gamma);
 
-slop0Descent=2
 
-b = -0.0001 *r0/slope0 ;  % initial step size
+
+b = -0.1 *r0/slope0Descent ;  % initial step size
 gamma=b ; [rb,UserVar,RunInfo,rForce1,rWork1,D21]=FuncDescent(gamma);
 
 CtrlVar.InfoLevelBackTrack=1000;
-CtrlVar.BacktrackingGammaMin=1e-13; 
+CtrlVar.BacktrackingGammaMin=1e-13; CtrlVar.LineSearchAllowedToUseExtrapolation=true;
 [gamma,r,BackTrackInfo]=BackTracking(slope0Descent,b,r0,rb,FuncDescent,CtrlVar);
+
+
+
+
+
