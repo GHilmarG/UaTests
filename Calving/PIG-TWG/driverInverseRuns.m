d3d8473c9a59cbd180c1be8516cf3d5347ac94fe
ghg@bas.ac.uk <ghg@bas.ac.uk>
2024-01-11 18:46:37 +0000

    local driver

--- Calving/PIG-TWG/driverInverseRuns.m
+++ Calving/PIG-TWG/driverInverseRuns.m
@@ -100,15 +100,20 @@ else
     UserVar.VelDataSet="";
 end
 
+% 50 iterations:
+% with smpd: 2395.4089373 sec = 39.9 min
+% without smpd: 3088 sec = 51.5 min   -> 28% increase in speed
+%
+%    with spmd: 2265
+% without spmd: 2742.6209298 sec, 21% increase,
 
-
-CtrlVar.Inverse.Iterations=5;
+CtrlVar.Inverse.Iterations=500;
 
 %delete(gcp('nocreate')); p=parpool('Threads',8); 
-delete(gcp('nocreate')); p=parpool('Processes',8); 
+% delete(gcp('nocreate')); p=parpool('Processes',8); 
 CtrlVar.Parallel.uvAssembly.spmd.isOn=true;
 CtrlVar.Parallel.uvAssembly.parfeval.isOn=false;
-CtrlVar.Parallel.isTest=true; 
+CtrlVar.Parallel.isTest=false; 
 
 if SubmitBathJobs
 
