b5272f03f002e4a03f21fea6e48adabd294c1c0f
U-C23000099\pcnj6 <ghg@bas.ac.uk>
2024-04-12 15:04:56 +0100

    RadialFlowStability convergence tests

--- RadialFlowStability/DefineGeometry.m
+++ RadialFlowStability/DefineGeometry.m
@@ -1,35 +1,40 @@
 
+
+
 function [UserVar,s,b,S,B,alpha]=DefineGeometry(UserVar,CtrlVar,MUA,time,FieldsToBeDefined)
 
-[rg,Domain_radius,shelf_width,h0,Bedrock,rho,rhow,g,thinice,ur,n,AGlen]=DefineMyParameters();
+% [rg,Domain_radius,shelf_width,h0,Bedrock,rho,rhow,g,thinice,ur,n,AGlen]=DefineMyParameters(UserVar);
+
+
+
 alpha = 0.0;  % zero slope of the coordinate system
 
 x=MUA.coordinates(:,1); y=MUA.coordinates(:,2);
 
-B=zeros(MUA.Nnodes,1)-Bedrock; %LS: Deep bedrock underneath the ocean to assure floatation.
+B=zeros(MUA.Nnodes,1)-UserVar.Bedrock; %LS: Deep bedrock underneath the ocean to assure floatation.
 S=B*0; %Ocean Surface, sea-level is zero
-b=S-thinice*(rho/rhow);  %The thin layer of ice/polymer everywhere in the computational domain.
-s=S+thinice*(1-rho/rhow);
+b=S-UserVar.thinice*(UserVar.rho/UserVar.rhow);  %The thin layer of ice/polymer everywhere in the computational domain.
+s=S+UserVar.thinice*(1-UserVar.rho/UserVar.rhow);
 
 %Now set the geometry on the boundary inner BC, r=rg:
-dx=rg/20;
-mask=x(MUA.Boundary.Nodes).^2+y(MUA.Boundary.Nodes).^2 <= rg*rg+dx; %Make sure all inner nodes are included, not the outer nodes
+dx=UserVar.rg/20;
+mask=x(MUA.Boundary.Nodes).^2+y(MUA.Boundary.Nodes).^2 <= UserVar.rg*UserVar.rg+dx; %Make sure all inner nodes are included, not the outer nodes
 nodesIn=MUA.Boundary.Nodes(mask);
 
-b(nodesIn)=0-h0*(rho/rhow); % This region is the circular shape with a constant thickness given by h0
-s(nodesIn)=h0*(1-rho/rhow);
+b(nodesIn)=0-UserVar.h0*(UserVar.rho/UserVar.rhow); % This region is the circular shape with a constant thickness given by h0
+s(nodesIn)=UserVar.h0*(1-UserVar.rho/UserVar.rhow);
 
 %Now definition of the geometric perturbation of the 2D shape:
-Ri=1.3*rg;
+Ri=1.3*UserVar.rg;
 k=9;
-Delta=Ri-rg;
+Delta=Ri-UserVar.rg;
 A=0.5*Delta; % A must be such that A/Delta<1 ! Otherwise the perturbation intercept into rg
 
 shape_p=Ri+A*cos(k*atan2(y,x)-pi/2);
 
 shape=sqrt((x.^2+y.^2))<=shape_p;
-b(shape)=0-h0*(rho/rhow);
-s(shape)=h0*(1-rho/rhow);
+b(shape)=0-UserVar.h0*(UserVar.rho/UserVar.rhow);
+s(shape)=UserVar.h0*(1-UserVar.rho/UserVar.rhow);
 hi=s-b;
 
 % save('my_hi','hi'); %just to have a quick initial look BEFORE adaptive mesh is done to improve it
--- RadialFlowStability/PostProcessingPlots.m
+++ RadialFlowStability/PostProcessingPlots.m
@@ -13,7 +13,7 @@ for I=1:2
 end
 
 
-%%
+%%  Original approach by Lielle
 
 Curdir=pwd;
 
@@ -34,8 +34,51 @@ for I=1:numel(Ffiles)
 end
 
 %%
-%
 
+%%   sequence of files
+
+Curdir=pwd;
+
+
+ResultsFileDirectory="C:\cygwin64\home\pcnj6\Ua\UaTests\RadialFlowStability\ResultsFiles";
+cd(ResultsFileDirectory)
+
+ResFiles=dir("*-MR250-AM0-RG0k015-n1-.mat") ;
+ResFiles=dir("*-MR250-AM0-RG0k015-n6-.mat") ;
+
+nstrive=1 ;
+
+for I=1:nstrive:numel(ResFiles)
+
+    load(ResFiles(I).name,"CtrlVar","F","MUA")
+
+    UaPlots(CtrlVar,MUA,F,"-uv-",FigureTitle="-uv-",CreateNewFigure=true)
+
+    UaPlots(CtrlVar,MUA,F,F.h,FigureTitle="-h-",CreateNewFigure=true)
+
+    speed=sqrt(F.ub.*F.ub+F.vb.*F.vb) ;
+    r=sqrt(F.x.*F.x+F.y.*F.y) ; 
+    [rSort,ir]=sort(r) ;
+
+    FindOrCreateFigure(" h(r) ")
+    plot(rSort,F.h(ir),".r")
+    xlabel("r",Interpreter="latex")
+    ylabel("h",Interpreter="latex")
+    title(sprintf("$t$=%g",F.time),Interpreter="latex")
+
+    FindOrCreateFigure(" h speed r ")
+    % Check mass conservatio by plottingL h*speed*r
+    hvr=F.h.*speed.*r ; 
+
+    plot(rSort,hvr(ir),".r")
+    xlabel("r",Interpreter="latex")
+    ylabel("hvr",Interpreter="latex")
+    title(sprintf("$t$=%g",F.time),Interpreter="latex")
+
+    drawnow
+    pause(0.1)
+
+end
 
 
 
