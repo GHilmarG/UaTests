c8fc4a1d8d19f1ff7fb142d3eff542f77e1236a7
Hilmar Gudmundsson <hilmar.gudmundsson@northumbria.ac.uk>
2025-12-29 14:13:54 +0000

    HO, brute for Hessian inversion added using full Hessian

---
+++ ACinversion/PlotGradVersHessianJ.m
@@ -0,0 +1,37 @@
+
+%%
+
+FileA="IR-CstartSetToMeanOfTrueC-AstartSetToMeanOfTrueA-MS25km-Tri3-MatGrad-.mat";
+FileB="IR-CstartSetToMeanOfTrueC-AstartSetToMeanOfTrueA-MS25km-Tri3-UaHess-.mat"; 
+
+FileA="IR-CstartSetToMeanOfTrueC-AstartSetToMeanOfTrueA-MS10km-Tri3-MatGrad-.mat";
+FileB="IR-CstartSetToMeanOfTrueC-AstartSetToMeanOfTrueA-MS10km-Tri3-UaHess-.mat"; 
+
+
+
+load(FileA,"RunInfo")
+
+
+fig=FindOrCreateFigure("Inverse Parameter Optimisation comparision"); clf(fig)
+
+semilogy(RunInfo.Inverse.Iterations,RunInfo.Inverse.J,'-bo','LineWidth',2,DisplayName="lBFGS")
+
+
+
+load(FileB,"RunInfo")
+
+hold on 
+semilogy(RunInfo.Inverse.Iterations,RunInfo.Inverse.J,'-r*','LineWidth',2,DisplayName="Hessian")
+
+ylabel("$J$",'interpreter','latex')
+xlabel("Iteration")
+lg=legend;
+
+
+MS=extractBetween(FileA,"-MS","-");
+
+exportgraphics(fig,"Hessian_lBFGS"+MS+".pdf")
+exportgraphics(fig,"Hessian_lBFGS"+MS+".png")
+
+
+%%
