a899cec42a570366ebed540d343c94787153fcc3
Hilmar Gudmundsson <hilmar.gudmundsson@northumbria.ac.uk>
2026-09-22 08:23:04 +0100

    HO

--- TestUa.m
+++ TestUa.m
@@ -1,8 +1,7 @@
 
-%%
-% 
 %
 % (beta)   26/06/2023 , 16/03/2024  , 01/08/2024 , 29/12/2024, 02/03/2025, 06/05/2025, 19/07/2025, 3/11,2025, 19/12/2025,
+% 22/12/2025, 20/07/2026, 02/09/2026, 21/09/2026
 %
 % To run the test do:
 %
@@ -31,7 +30,7 @@ function tests = TestUa
       
       f={@testCrack,@testPIGdiagnostic,@testPIGtransient,@testMassBalanceFeedback,@test1dIceStream,@test1dIceShelf,@testGaussPeak,@testFreeSlipBCs,@testMassConservationPeaks} ;
 
-       f={@testPIGtransient}    
+      % f={@testPIGtransient}    
       % f={@testPIGdiagnostic}    
 
     tests = functiontests(f);
@@ -66,23 +65,8 @@ function testPIGdiagnostic(testCase)
     UserVar.RunType='Forward-Diagnostic'; 
     UserVar=Ua(UserVar) ;
     cd ..
-<<<<<<< HEAD
-<<<<<<< HEAD
-    actSolution= UserVar.Test.Norm.actValue 
-    expSolution = UserVar.Test.Norm.expValue 
-=======
-    actSolution= UserVar.Test.Norm.actValue ;
-    % expSolution = 95982.7181182457;  % this was with the old bedmap2 data
-    % expSolution = 9757675340.83092 ;   % this is with the new Bedmachine data, 10/09/2021
-    % expSolution = 202912           ;     % this is with the new Bedmachine data and a new boundary, 01/11/2021
-    expSolution = UserVar.Test.Norm.expValue ;
- 
-
->>>>>>> 9adf3c17344d4f3920374a01c16b0d2557933834
-=======
     actSolution= UserVar.Test.Norm.actValue ;
     expSolution = UserVar.Test.Norm.expValue ;
->>>>>>> ce92ce6c73697172ada4f8b36b6958b313b0dd23
     verifyEqual(testCase,actSolution,expSolution,'RelTol',1e-4)
   
 
@@ -280,3 +264,14 @@ end
 % 
 % cd WavyChannel ; clear ; close all ; Ua ; cd ..
 % 
+
+
+
+
+
+
+
+
+
+
+
